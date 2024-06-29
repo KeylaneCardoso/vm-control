d7aaf5a2d13216d3250e9c2afbb030a932ceab83
Keylane Cardoso <keylanecardoso28@gmail.com>
2024-06-29 16:06:58 -0300

    feat: inserção dos códigos do dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -8,8 +8,61 @@ import { HeaderComponent } from '../../components/header/header.component';
   standalone: true,
   imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  userName = "Keylane";
 
+  ngOnInit(){
+    this.RenderPieChart();
+    this.RenderBarChart()
+  }
+
+  RenderPieChart(){
+    // const pieChart = new Chart( "piechart", {
+    //   type: 'pie',
+    //   data: {
+    //     labels: [
+    //       "VM's em uso",
+    //       "VM's disponiveis"
+    //     ],
+    //     datasets: [{
+    //       label: "Maquinas Virtuais",
+    //       data: [3, 10],
+    //       backgroundColor: [
+    //         '#00BBFF',
+    //         '#E8FFFE'
+    //       ],
+    //       hoverOffset: 4
+    //     }]
+    //   }
+    // })
+
+  }
+
+  RenderBarChart(){
+    // const barChart = new Chart( "barchart", {
+    //   type: 'bar',
+    //   data: {
+    //     labels: ["RUNNING", "PAUSED", "STOP"],
+    //     datasets: [{
+    //       label: "Status das VM's",
+    //       data: [1, 2, 3],
+    //       backgroundColor: [
+    //         '#FF8A08',
+    //         '#FFBE76',
+    //         '#FFB908'
+    //       ],
+    //       borderRadius: 5,
+    //     }]
+    //   },
+    //   options: {
+    //     indexAxis: 'y',
+    //     // gridLinesX: {
+    //     //   display: false
+    //     // }
+    //   },  
+    // })
+
+  }
 }
