a14386cb457fff88795083e2abd2ca83096e1120
Keylane Cardoso <keylanecardoso28@gmail.com>
2024-06-29 16:23:06 -0300

    feat: adição das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,24 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ListingVmComponent } from './pages/listing-vm/listing-vm.component';
+import { RegisterVmComponent } from './pages/register-vm/register-vm.component';
 
 export const routes: Routes = [
     {
         path: "",
-        component: DashboardComponent
+        component: LoginComponent
     },
     {
         path: "home",
         component: DashboardComponent
+    },
+    {
+        path: "visualizarVM",
+        component: ListingVmComponent
+    },
+    {
+        path: "cadastrarVM",
+        component: RegisterVmComponent
     },
 ];
