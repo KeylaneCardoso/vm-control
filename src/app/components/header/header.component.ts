0d371a71c63994a2c58fab912a9da40e0e93d8f6
Keylane Cardoso <keylanecardoso28@gmail.com>
2024-06-30 23:29:27 -0300

    feat: adição de botão para logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent implements OnInit {
-  userName = "Keylane"
+export class HeaderComponent{
 
-  ngOnInit() {
-      
+  constructor(private localStorageService: LocalStorageService, private router: Router) { }
+
+  userName = this.BuscarDados();
+
+  isOpen = false;
+
+  expand(){
+    this.isOpen = !this.isOpen;
   }
 
+  logout(){
+    this.router.navigate(['']);
+    alert("Logout realizado com sucesso!")
+  }
 
+  BuscarDados(): string{
+    const userDataString = this.localStorageService.getItem('userData');
+    if (userDataString === null || userDataString === undefined) {
+      console.error('Dados do usuário não encontrados no localStorage');
+      return 'error';
+    }
+    const userData = JSON.parse(userDataString);
+    const userName = userData.name;
+    console.log(userName)
+
+    return userName;
+  }
 }
